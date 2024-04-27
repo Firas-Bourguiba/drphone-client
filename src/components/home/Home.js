f301be43cc5dedded1ebc6c10a7b6db4e7121d42
firas bourguiba <fbourguiba18@gmail.com>
2024-04-27 13:04:41 +0100

    dd

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
             marginTop: 50,
           }}
         >
-          <h1>devops</h1>
+          <h1>test</h1>
           <div onClick={() => navigateTo()}>
             <h5>View More</h5>
           </div>
